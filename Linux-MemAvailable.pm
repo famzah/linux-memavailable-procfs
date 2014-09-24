ca40094c7313de7f5a22001ce966fe552ed36acc
Ivan Zahariev <famzah@famzah.net>
2014-09-24 13:37:51 +0300

    Initial commit

---
+++ Linux-MemAvailable.pm
@@ -0,0 +1,210 @@
+#!/usr/bin/perl
+
+package Linux::MemAvailable;
+
+=head1 NAME
+
+Linux::MemAvailable - Backport of the /proc/meminfo "MemAvailable" metric for Linux kernels before 3.14.
+
+=head1 SYNOPSIS
+
+	require 'Linux-MemAvailable.pm';
+
+	#Linux::MemAvailable::set_debug();
+
+	my ($avail, $meminfo) = Linux::MemAvailable::calculate();
+
+	my $total = $meminfo->{'MemTotal'};
+	printf("Available: %d / %d kB (%.0f%%)\n", $avail, $total, $avail/$total*100);
+
+=head1 NOTE
+
+Kernel commit in C:
+  https://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/?id=34e431b0ae398fc54ea69ff85ec700722c9da773
+
+WARNING: This module uses die() on errors which should never happen. Wrap with eval() if needed.
+
+=head1 METHODS
+
+calculate() - returns a list with:
+
+	(
+		[0] => the calculated value of MemAvailable in kB,
+		[1] => hashref containing the parsed "/proc/meminfo",
+	)
+
+Note that you can call calculate() by supplying ($meminfo_lines_aref, $zoneinfo_lines_aref) as function arguments. In this case the calculation uses the provided array refs as a source for "/proc/meminfo" and "/proc/zoneinfo" respectively.
+
+set_debug() - enables debug messages on the STDOUT
+
+=head1 EXAMPLES
+
+Easy way to consume memory in user-space:
+  perl -e '$s = ""; for ($i = 0; $i < 2000; ++$i) { $s .= "A" x 1048576 }; print "Done\n"; sleep(6000);'
+
+Easy way to put contents into the page cache buffer:
+  find /usr -type f -print0|xargs -r -0 cat >/dev/null
+
+NOTE: If you want to test when the system will start swapping, simply disable swap and then try to allocate lots of memory until you get an out-of-memory error.
+
+=head1 AUTHOR
+
+Ivan Zahariev (famzah)
+
+=cut
+
+use strict;
+use warnings;
+
+use POSIX;
+
+my $debug = 0;
+
+sub set_debug {
+	$debug = 1;
+}
+
+sub get_wmark_low_in_pages {
+	my ($zoneinfo_lines) = @_;
+	my $line;
+	my @lines;
+	my $got_zone_hdr = 0;
+	my $got_low_val = 0;
+	my $wmark_low = 0;
+	my $v;
+
+	if (!defined($zoneinfo_lines)) {
+		open(F, '<', '/proc/zoneinfo') or die("open('/proc/zoneinfo'): $!");
+		@lines = <F>;
+		close(F) or die("close('/proc/zoneinfo'): $!");
+	} else {
+		@lines = @{$zoneinfo_lines};
+	}
+
+	foreach $line (@lines) {
+		if ($line =~ /^Node\s+\d+,\s+zone\s+/) {
+			$got_zone_hdr = 1;
+			$got_low_val = 0;
+
+			next;
+		}
+		if ($line =~ /^\s+low\s+(\d+)\s*$/) {
+			$v = $1;
+
+			if (!$got_zone_hdr) {
+				die("ERROR: Got 'low' before we encountered a 'zone' start");
+			}
+			if ($got_low_val) {
+				die("ERROR: Got 'low' for the second time");
+			}
+
+			$got_low_val = 1;
+			$wmark_low += $v;
+
+			next;
+		}
+	}
+
+	return $wmark_low; # /proc/zoneinfo is in "pages", not in kB
+}
+
+sub parse_meminfo {
+	my ($meminfo_lines) = @_;
+	my $line;
+	my $data = {};
+	my @lines;
+
+	if (!defined($meminfo_lines)) {
+		open(F, '<', '/proc/meminfo') or die("open('/proc/meminfo'): $!");
+		@lines = <F>;
+		close(F) or die("close('/proc/meminfo'): $!");
+	} else {
+		@lines = @{$meminfo_lines};
+	}
+
+	foreach $line (@lines) {
+		#print "/proc/meminfo: $line" if ($debug);
+		if ($line =~ /^HugePages_/) {
+			next; # those lines don't match the regexp below, and we don't need them anyway
+		}
+		if ($line !~ /^([^:]+):\s+(\d+)\s+kB\s*$/) {
+			die("ERROR: Unable to parse a line in '/proc/meminfo': $line");
+		}
+		$data->{$1} = $2;
+	}
+
+	return $data;
+}
+
+sub get_v {
+	my ($data, $k) = @_;
+
+	if (!exists($data->{$k}) || !defined($data->{$k})) {
+		die("ERROR: Key '$k' not found in '/proc/meminfo'");
+	}
+
+	return $data->{$k};
+}
+
+sub min ($$) { $_[$_[0] > $_[1]] }; # http://www.perlmonks.org/?node_id=406883
+
+sub calculate {
+	my ($meminfo_lines, $zoneinfo_lines) = @_;
+
+	my $wmark_low;
+	my $meminfo;
+	my $available;
+	my $pagecache;
+	my ($lru_active_file, $lru_inactive_file);
+	my $memfree;
+	my $slab_reclaimable;
+
+	my $pagesize = POSIX::sysconf(POSIX::_SC_PAGESIZE);
+
+	if (!defined($pagesize)) {
+		die("ERROR: Unable to determine the memory 'pagesize'");
+	}
+
+	$wmark_low = get_wmark_low_in_pages($zoneinfo_lines);
+	print "wmark_low: $wmark_low pages\n" if ($debug);
+	$wmark_low *= $pagesize; # convert to Bytes
+	$wmark_low = sprintf('%.0f', $wmark_low / 1024); # convert to kB
+	print "wmark_low: $wmark_low kB\n" if ($debug);
+
+	$meminfo = parse_meminfo($meminfo_lines);
+
+	$memfree = get_v($meminfo, 'MemFree');
+	print "MemFree: $memfree kB\n" if ($debug);
+
+	$available = $memfree - $wmark_low;
+
+	$lru_active_file = get_v($meminfo, 'Active(file)');
+	$lru_inactive_file = get_v($meminfo, 'Inactive(file)');
+	print "Active(file): $lru_active_file kB\n" if ($debug);
+	print "Inactive(file): $lru_inactive_file kB\n" if ($debug);
+
+	$pagecache = $lru_active_file + $lru_inactive_file;
+	$pagecache -= min($pagecache / 2, $wmark_low);
+	$pagecache = sprintf('%.0f', $pagecache); # round
+	$available += $pagecache;
+
+	$slab_reclaimable = get_v($meminfo, 'SReclaimable');
+	print "SReclaimable: $slab_reclaimable kB\n" if ($debug);
+
+	$available += $slab_reclaimable - min($slab_reclaimable, $wmark_low);
+
+	if ($available < 0) {
+		$available = 0;
+	}
+
+	if ($debug) {
+		print "MemAvailable: $available kB\n";
+	}
+
+	return (
+		$available,
+		$meminfo,
+	);
+}
+
+1;
---
+++ free.pl
@@ -0,0 +1,169 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Getopt::Std;
+use File::Basename;
+use POSIX;
+
+require 'Linux-MemAvailable.pm';
+
+# super-globals modified by the command-line arguments
+my $scale = 1024; # default scale is KB
+my $show_perc = 0;
+my $show_extended = 0;
+
+sub fmt($$$) {
+	my ($value, $total, $is_total) = @_;
+	my $perc_value;
+
+	if (!defined($value)) {
+		return 'N/A';
+	}
+
+	if ($total != 0) {
+		$perc_value = sprintf('%.0f%%', ($value / $total) * 100);
+	} else {
+		$perc_value = '-';
+	}
+	
+	$value *= 1024; # kB -> bytes ("/proc/meminfo" returns in "kB")
+	$value /= $scale;
+	#$value = sprintf('%.0f', $value);
+	$value = POSIX::floor($value); # the original "free" tool rounds this way
+
+	if (!$show_perc) {
+		return $value;
+	} else {
+		if ($is_total) {
+			return $value; # the "total" is always returned as a raw value
+		} else { # the other values are returned as a percentage
+			return $perc_value;
+		}
+	}
+}
+
+sub HELP_MESSAGE() {
+	my $myname = basename($0);
+	my $help = <<EOF;
+Usage: $myname [-b|-k|-m|-g] [-p] [-e]
+  -b,-k,-m,-g  show output in bytes, KB, MB, or GB
+  -p           show output in percentage
+  -e           show extended memory usage info
+EOF
+	die($help);
+}
+
+sub VERSION_MESSAGE() {
+	HELP_MESSAGE();
+}
+
+sub parse_cmd_args() {
+	my %opts;
+	if (!getopts('bkmgpe', \%opts)) {
+		HELP_MESSAGE();
+	}
+
+	if (defined($opts{'b'})) {
+		$scale = 1;
+	}
+	if (defined($opts{'m'})) {
+		$scale = 1024 * 1024;
+	}
+	if (defined($opts{'g'})) {
+		$scale = 1024 * 1024 * 1024;
+	}
+
+	if (defined($opts{'p'})) {
+		$show_perc = 1;
+	}
+
+	if (defined($opts{'e'})) {
+		$show_extended = 1;
+	}
+}
+
+parse_cmd_args();
+
+my ($avail, $meminfo) = Linux::MemAvailable::calculate();
+
+my @mem_row_header = (
+	'total', 'used', 'free', 'anonymous', 'kernel', 'caches', 'others'
+);
+
+my $mem_row_data = {
+	'total' => $meminfo->{'MemTotal'},
+	'used' => $meminfo->{'MemTotal'} - $meminfo->{'MemFree'},
+	'free' => $meminfo->{'MemFree'},
+	'anonymous' => $meminfo->{'Active(anon)'} + $meminfo->{'Inactive(anon)'},
+	'kernel' => $meminfo->{'SUnreclaim'} + $meminfo->{'PageTables'} + $meminfo->{'KernelStack'},
+	'caches' => $meminfo->{'Active(file)'} + $meminfo->{'Inactive(file)'} + $meminfo->{'SReclaimable'},
+};
+$mem_row_data->{'others'} = $mem_row_data->{'total'} -
+	$mem_row_data->{'free'} -
+	$mem_row_data->{'anonymous'} -
+	$mem_row_data->{'kernel'} -
+	$mem_row_data->{'caches'};
+
+my $minus_caches_data = {
+	'used' => $mem_row_data->{'used'} - $mem_row_data->{'caches'},
+	'free' => $mem_row_data->{'free'} + $mem_row_data->{'caches'},
+};
+
+my $swap_data = {
+	'total' => $meminfo->{'SwapTotal'},
+	'used' => $meminfo->{'SwapTotal'} - $meminfo->{'SwapFree'},
+	'free' => $meminfo->{'SwapFree'},
+};
+
+my @mem_row_values = ();
+for my $k (@mem_row_header) {
+	push(@mem_row_values, fmt($mem_row_data->{$k}, $mem_row_data->{'total'}, $k eq 'total'));
+}
+
+my $first_header_fmt = "%-7s %10s %10s %10s %10s %10s %10s %10s\n";
+printf($first_header_fmt, '', @mem_row_header);
+printf($first_header_fmt, 'Mem:', @mem_row_values);
+printf(
+	"%-18s %10s %10s\n",
+	'  -/+ caches',
+	fmt($minus_caches_data->{'used'}, $mem_row_data->{'total'}, 0),
+	fmt($minus_caches_data->{'free'}, $mem_row_data->{'total'}, 0)
+);
+printf(
+	"%-7s %10s %10s %10s\n",
+	'Swap:',
+	fmt($swap_data->{'total'}, $swap_data->{'total'}, 1),
+	fmt($swap_data->{'used'}, $swap_data->{'total'}, 0),
+	fmt($swap_data->{'free'}, $swap_data->{'total'}, 0)
+);
+
+if (!$show_extended) {
+	exit(0); # XXX: Exit here if extended info was not requested
+}
+
+my @extended_row_header = (
+	'Buffers', 'Cached', 'SwapCached', 'Shmem', 'AnonPages', 'Mapped',
+	'Unevict+Mlocked', 'Dirty+Writeback',
+	'NFS+Bounce'
+);
+my $extended_data = {
+	'Buffers' => $meminfo->{'Buffers'},
+	'Cached' => $meminfo->{'Cached'},
+	'SwapCached' => $meminfo->{'SwapCached'},
+	'Shmem' => $meminfo->{'Shmem'},
+	'AnonPages' => $meminfo->{'AnonPages'},
+	'Mapped' => $meminfo->{'Mapped'},
+	'Unevict+Mlocked' => $meminfo->{'Unevictable'} + $meminfo->{'Mlocked'},
+	'Dirty+Writeback' => $meminfo->{'Dirty'} + $meminfo->{'Writeback'} +
+		$meminfo->{'WritebackTmp'},
+	'NFS+Bounce' => $meminfo->{'NFS_Unstable'} + $meminfo->{'Bounce'},
+};
+
+printf("\nExtended memory usage info:\n");
+foreach my $k (@extended_row_header) {
+	printf(
+		"%-18s %10s\n",
+		"  $k", fmt($extended_data->{$k}, $mem_row_data->{'total'}, 0)
+	);
+}
