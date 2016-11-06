cfb776d989a82bc2a37bd026a30608ec908a5bf9
Ivan Zahariev <famzah@famzah.net>
2016-11-06 11:25:08 +0200

    Backport the new slightly different algorithm for "MemAvailable" used by Linux kernel 4.8+ (issue #2)

--- Linux-MemAvailable.pm
+++ Linux-MemAvailable.pm
@@ -11,6 +11,7 @@ Linux::MemAvailable - Backport of the /proc/meminfo "MemAvailable" metric for Li
 	require 'Linux-MemAvailable.pm';
 
 	#Linux::MemAvailable::set_debug();
+	#Linux::MemAvailable::set_old_calc();
 
 	my ($avail, $meminfo) = Linux::MemAvailable::calculate();
 
@@ -59,11 +60,183 @@ use warnings;
 use POSIX;
 
 my $debug = 0;
+my $old_calc = 0;
 
 sub set_debug {
 	$debug = 1;
 }
+sub set_old_calc {
+	$old_calc = 1;
+}
 
+sub _get_empty_zone_struct {
+	return {
+		'zone_header' => {
+				'regexp' => '^(Node\s+\d+,\s+zone\s+.+)$',
+				'value' => undef,
+		},
+		'high_wmark_pages' => {
+				'regexp' => '^\s+high\s+(\d+)\s*$',
+				'value' => undef,
+		},
+		'managed_pages' => {
+				'regexp' => '^\s+managed\s+(\d+)\s*$',
+				'value' => undef,
+		},
+		'lowmem_reserve' => {
+				'regexp' => '^\s+protection:\s+\(([0-9, ]+)\)\s*$',
+				'value' => undef,
+		},
+	};
+}
+
+sub _zone_end {
+	my ($zone, $all_zones) = @_;
+	my ($key, $v);
+	my @arr;
+	my $node_id;
+
+	foreach $key (keys %{$zone}) {
+		if (!defined($zone->{$key}->{'value'})) {
+			die("ERROR: Zone parsing is incomplete; missing key='$key'");
+		}
+	}
+	@arr = split(/, /, $zone->{'lowmem_reserve'}->{'value'});
+	foreach $v (@arr) {
+		if ($v !~ /^\d+$/) {
+			die("Invalid value for 'lowmem_reserve': $v");
+		}
+	}
+	$zone->{'lowmem_reserve'}->{'value'} = \@arr;
+
+	# https://www.kernel.org/doc/gorman/html/understand/understand005.html
+	if ($zone->{'zone_header'}->{'value'} !~ /^Node\s+(\d+),\s+zone\s+/) {
+		die("Unable to parse the zone header: ".$zone->{'zone_header'}->{'value'});
+	}
+	$node_id = $1;
+
+	if (!exists($all_zones->[$node_id])) {
+		$all_zones->[$node_id] = [];
+	}
+	push(@{$all_zones->[$node_id]}, $zone);
+}
+
+sub parse_proc_zoneinfo {
+	my ($zoneinfo_lines, $all_zones) = @_;
+	my $zone;
+	my (@lines, $line);
+	my ($key, $v);
+	my $regexp;
+	my $line_num = 0;
+
+	if (!defined($zoneinfo_lines)) {
+		open(F, '<', '/proc/zoneinfo') or die("open('/proc/zoneinfo'): $!");
+		@lines = <F>;
+		close(F) or die("close('/proc/zoneinfo'): $!");
+	} else {
+		@lines = @{$zoneinfo_lines};
+	}
+
+	$zone = _get_empty_zone_struct();
+	foreach $line (@lines) {
+		++$line_num;
+		foreach $key (keys %{$zone}) {
+			$regexp = $zone->{$key}->{'regexp'};
+			if ($line =~ /$regexp/) {
+				$v = $1;
+				if ($key eq 'zone_header') {
+					if ($line_num > 1) {
+						_zone_end($zone, $all_zones);
+					}
+					$zone = _get_empty_zone_struct();
+				}
+				if (defined($zone->{$key}->{'value'})) {
+					die("ERROR: Got Zone key='$key' for the second time");
+				}
+				if ($key ne 'zone_header' && !defined($zone->{'zone_header'}->{'value'})) {
+					die("ERROR: Got Zone key='$key' before we encountered a Zone start");
+				}
+				$zone->{$key}->{'value'} = $v;
+			}
+		}
+	}
+	_zone_end($zone, $all_zones);
+}
+
+sub get_max_nr_zones {
+	my ($all_zones) = @_;
+	my $zone;
+	my $MAX_NR_ZONES = undef;
+	my $val;
+	my $node_zones;
+
+	foreach $node_zones (@{$all_zones}) {
+		foreach $zone (@{$node_zones}) {
+			$val = scalar @{$zone->{'lowmem_reserve'}->{'value'}};
+			if (!defined($MAX_NR_ZONES)) {
+				$MAX_NR_ZONES = $val;
+			}
+			if ($MAX_NR_ZONES != $val) {
+				die("ERROR: Got different count for MX_NR_ZONES: $val");
+			}
+		}
+	}
+
+	print "MAX_NR_ZONES: $MAX_NR_ZONES\n" if ($debug);
+	return $MAX_NR_ZONES;
+}
+
+# https://github.com/famzah/linux-memavailable-procfs/issues/2
+# https://github.com/torvalds/linux/blob/6aa303defb7454a2520c4ddcdf6b081f62a15890/mm/page_alloc.c#L6559
+sub calculate_totalreserve_pages {
+	my ($all_zones) = @_;
+	my $zone;
+	my $MAX_NR_ZONES = get_max_nr_zones($all_zones);
+	my $reserve_pages = 0;
+	my ($i, $j);
+	my $node_zones;
+	my $max;
+
+	foreach $node_zones (@{$all_zones}) {
+		for ($i = 0; $i < $MAX_NR_ZONES; $i++) {
+			if (!defined($node_zones->[$i])) {
+				# The Linux kernel implementation loops over not active zones too?
+				# Maybe they are zero-initialized, since the result is the same
+				# as if we stop iterating MAX_NR_ZONES.
+				# Or maybe we get MAX_NR_ZONES inappropriately.
+				#
+				# Nevertheless, iterating only the existing zones seems correct,
+				# and also gives the same results as the Linux kernel implementation.
+
+				last;
+			}
+
+			$zone = $node_zones->[$i];
+			$max = 0;
+
+			# find valid and maximum lowmem_reserve in the zone
+			for ($j = $i; $j < $MAX_NR_ZONES; $j++) {
+				if ($zone->{'lowmem_reserve'}->{'value'}->[$j] > $max) {
+					$max = $zone->{'lowmem_reserve'}->{'value'}->[$j];
+				}
+			}
+
+			# we treat the high watermark as reserved pages
+			$max += $zone->{'high_wmark_pages'}->{'value'};
+
+			if ($max > $zone->{'managed_pages'}->{'value'}) {
+				$max = $zone->{'managed_pages'}->{'value'};
+			}
+
+			print $zone->{'zone_header'}->{'value'}.": reserve_pages=$max\n" if ($debug);
+			$reserve_pages += $max;
+		}
+	}
+
+	return $reserve_pages; # totalreserve_pages
+}
+
+# TODO: Refactor to use parse_proc_zoneinfo()
 sub get_wmark_low_in_pages {
 	my ($zoneinfo_lines) = @_;
 	my $line;
@@ -148,10 +321,28 @@ sub get_v {
 
 sub min ($$) { $_[$_[0] > $_[1]] }; # http://www.perlmonks.org/?node_id=406883
 
+sub pages_to_kb {
+	my ($label, $value) = @_;
+	my $pagesize = POSIX::sysconf(POSIX::_SC_PAGESIZE);
+
+	if (!defined($pagesize)) {
+		die("ERROR: Unable to determine the memory 'pagesize'");
+	}
+
+	print "$label: $value pages\n" if ($debug);
+	$value *= $pagesize; # convert to Bytes
+	$value = sprintf('%.0f', $value / 1024); # convert to kB
+	print "$label: $value kB\n" if ($debug);
+
+	return $value;
+}
+
 sub calculate {
 	my ($meminfo_lines, $zoneinfo_lines) = @_;
 
 	my $wmark_low;
+	my $totalreserve_pages;
+	my $all_zones = [];
 	my $meminfo;
 	my $available;
 	my $pagecache;
@@ -159,24 +350,26 @@ sub calculate {
 	my $memfree;
 	my $slab_reclaimable;
 
-	my $pagesize = POSIX::sysconf(POSIX::_SC_PAGESIZE);
+	parse_proc_zoneinfo($zoneinfo_lines, $all_zones);
 
-	if (!defined($pagesize)) {
-		die("ERROR: Unable to determine the memory 'pagesize'");
-	}
+	$totalreserve_pages = calculate_totalreserve_pages($all_zones);
+	$totalreserve_pages = pages_to_kb('totalreserve_pages', $totalreserve_pages);
 
 	$wmark_low = get_wmark_low_in_pages($zoneinfo_lines);
-	print "wmark_low: $wmark_low pages\n" if ($debug);
-	$wmark_low *= $pagesize; # convert to Bytes
-	$wmark_low = sprintf('%.0f', $wmark_low / 1024); # convert to kB
-	print "wmark_low: $wmark_low kB\n" if ($debug);
+	$wmark_low = pages_to_kb('wmark_low', $wmark_low);
 
 	$meminfo = parse_meminfo($meminfo_lines);
 
 	$memfree = get_v($meminfo, 'MemFree');
 	print "MemFree: $memfree kB\n" if ($debug);
 
-	$available = $memfree - $wmark_low;
+	# https://github.com/famzah/linux-memavailable-procfs/issues/2
+	# https://github.com/torvalds/linux/commit/84ad5802a33a4964a49b8f7d24d80a214a096b19
+	if ($old_calc) {
+		$available = $memfree - $wmark_low;
+	} else {
+		$available = $memfree - $totalreserve_pages;
+	}
 
 	$lru_active_file = get_v($meminfo, 'Active(file)');
 	$lru_inactive_file = get_v($meminfo, 'Inactive(file)');
